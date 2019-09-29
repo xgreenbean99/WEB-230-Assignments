643e57dfe49a30d44266a37692ec2559faad6b69
xgreenbean99 <47220549+xgreenbean99@users.noreply.github.com>
2019-09-29 17:20:19 -0400

    Add files via upload

---
+++ switch/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$day = 'sunday';
+
+switch($day){
+    case 'saturday':
+    case 'sunday':
+        echo 'it is a weekend';
+    break;
+
+    default:
+        echo 'not a weekend';
+    break;
+}
+
+
+
+?>
